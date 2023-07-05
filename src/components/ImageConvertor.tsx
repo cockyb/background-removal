566e53a95e67a41a1c67b063dbdb6a2103d046dd
owen <56942459+cockyb@users.noreply.github.com>
2023-07-05 16:21:06 +0900

    feat: image convert

--- src/components/ImageConvertor.tsx
+++ src/components/ImageConvertor.tsx
@@ -1,29 +1,104 @@
+import type { Config } from '@imgly/background-removal';
 import imglyRemoveBackground from '@imgly/background-removal';
-import { useState } from 'react';
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+import { SlClose } from 'react-icons/sl';
+import { ImSpinner } from 'react-icons/im';
 
 interface ImageConvertorProps {
     image: string;
+    fileName: string;
+    onClickClose: () => void;
 }
 
-// let image_src: ImageData | ArrayBuffer | Uint8Array | Blob | URL | string = ...;
+const TABS = ['원본', '제거된 배경'] as const;
+type Tab = (typeof TABS)[number];
 
 export default function ImageConvertor(props: ImageConvertorProps) {
-    const { image: originImage } = props;
+    const { image: originImage, fileName: originFileName, onClickClose } = props;
     const [convertedImage, setConvertedImage] = useState<string>('');
+    const [fileName, setFileName] = useState<string>('');
+    const [tab, setTab] = useState<Tab>('원본');
 
-    const onClickConvert = async () => {
-        const blob = await imglyRemoveBackground(originImage);
-        const url = URL.createObjectURL(blob);
+    const onClickReConvert = async () => {
+        setConvertedImage('');
+        const url = await convertImage(originImage);
         setConvertedImage(url);
     };
+
+    const convertImage = async (image: string) => {
+        const config: Config = {
+            publicPath: 'https://dq2ft8ldnul0t.cloudfront.net/assets/', // path to the wasm files
+        };
+
+        const blob = await imglyRemoveBackground(image, config);
+        const url = URL.createObjectURL(blob);
+        return url;
+    };
+
+    useEffect(() => {
+        (async () => {
+            const url = await convertImage(originImage);
+            setConvertedImage(url);
+        })();
+    }, [originImage]);
+
+    useEffect(() => {
+        setFileName(originFileName);
+    }, [originFileName]);
+
     return (
-        <div>
-            <img src={originImage} alt="" />
-            <button onClick={onClickConvert}>변환</button>
-            <img src={convertedImage} alt="" />
-            <a href={convertedImage} download="image.png">
-                다운로드
-            </a>
+        <div className="px-4 py-3 border border-gray-300 rounded-sm shadow-sm flex flex-col justify-between gap-4">
+            <div className="flex gap-3">
+                {TABS.map((t) => (
+                    <button key={t} className={clsx('text-center', { 'text-blue-400 font-bold': tab === t })} onClick={() => setTab(t)}>
+                        {t}
+                    </button>
+                ))}
+                <button className="ml-auto" onClick={onClickClose}>
+                    <SlClose className="w-5 h-5 hover:text-gray-400 duration-150" />
+                </button>
+            </div>
+            {tab === '원본' && <img className="w-full aspect-square object-cover" src={originImage} alt="" />}
+            {tab === '제거된 배경' && <ConvertedImage image={convertedImage} />}
+
+            <label className="flex gap-2 items-center justify-center flex-wrap">
+                <span className="text-gray-500">파일명</span>
+                <input
+                    className="w-auto border border-gray-300 focus:outline-none focus:border-blue-400 px-3 py-1 rounded-md"
+                    type="text"
+                    value={fileName}
+                    onChange={(e) => setFileName(e.target.value)}
+                />
+                <span>.png</span>
+            </label>
+
+            <div className="flex gap-3 items-center justify-center flex-wrap">
+                <button className="px-4 py-2 text-gray-500 rounded-md border-gray-400 border text-sm" onClick={onClickReConvert}>
+                    다시변환
+                </button>
+                <a
+                    className={clsx('px-4 py-2 text-white bg-blue-400 rounded-md text-sm', {
+                        'opacity-50 cursor-not-allowed pointer-events-none': !convertedImage,
+                    })}
+                    href={convertedImage}
+                    download={fileName}
+                >
+                    다운로드
+                </a>
+            </div>
         </div>
     );
+}
+
+function ConvertedImage(props: { image: string }) {
+    const { image } = props;
+
+    if (!image)
+        return (
+            <div className="w-full aspect-square flex items-center justify-center">
+                <ImSpinner className="w-6 h-6 animate-spin" />
+            </div>
+        );
+    return <img className="w-full aspect-square object-cover" src={image} alt="" />;
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,39 @@
+import clsx from 'clsx';
 import ImageConvertor from 'components/ImageConvertor';
-import type { ChangeEventHandler } from 'react';
+import type { ChangeEventHandler, DragEventHandler } from 'react';
 import { useState } from 'react';
+import { SlCloudUpload } from 'react-icons/sl';
 
-// type ImageSource = ImageData | ArrayBuffer | Uint8Array | Blob | URL | string;
+type ImageInfo = {
+    blob: string;
+    name: string;
+};
 
 export default function Home() {
-    const [images, setImages] = useState<string[]>([]);
+    const [images, setImages] = useState<ImageInfo[]>([]);
+    const [dragEnter, setDragEnter] = useState<boolean>(false);
 
     const handleFileChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
         const files = e.target.files;
         if (!files) return;
+        handleImages(files);
+    };
 
+    const handleDrop: DragEventHandler<HTMLLabelElement> = async (e) => {
+        e.preventDefault();
+        setDragEnter(false);
+        handleImages(e.dataTransfer.files);
+    };
+
+    const handleImages = async (files: FileList) => {
         const newImages = await Promise.all(
             [...files].map(async (file) => {
-                const url = URL.createObjectURL(file);
-                const image = await fetch(url).then((res) => res.blob());
-                return URL.createObjectURL(image);
+                const blob = await new Promise<string>((resolve) => {
+                    const reader = new FileReader();
+                    reader.onload = (e) => resolve(e.target?.result as string);
+                    reader.readAsDataURL(file);
+                });
+                return { blob: blob, name: file.name };
             })
         );
 
@@ -24,14 +42,50 @@ export default function Home() {
 
     return (
         <>
-            <input type="file" value="" multiple onChange={handleFileChange} />
-            <ul>
-                {images.map((image) => (
-                    <li key={image}>
-                        <ImageConvertor image={image} />
-                    </li>
-                ))}
-            </ul>
+            <section className="py-6">
+                <label
+                    htmlFor="dropzone-file"
+                    className={clsx(
+                        'mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-blue-400 bg-white p-6 text-center',
+                        {
+                            'bg-gray-200 animate-pulse': dragEnter,
+                        }
+                    )}
+                    onDragEnter={() => setDragEnter(true)}
+                    onDragExit={() => setDragEnter(false)}
+                    onDrop={handleDrop}
+                    onDragOver={(e) => {
+                        e.preventDefault();
+                        setDragEnter(true);
+                    }}
+                >
+                    <SlCloudUpload className="w-8 h-8 text-blue-400" />
+
+                    <h2 className="mt-4 text-xl font-medium text-gray-700 tracking-wide">이미지 파일</h2>
+
+                    <p className="mt-2 text-gray-500 tracking-wide">
+                        이미지 파일을 여기에 끌어다 놓거나 <span className="text-blue-500">클릭</span>하여 업로드하세요.
+                    </p>
+
+                    <input id="dropzone-file" type="file" className="hidden" multiple onChange={handleFileChange} />
+                </label>
+            </section>
+            <section className="py-6">
+                {images.length > 0 && (
+                    <ul className="grid grid-cols-4 gap-3">
+                        {images.map(({ blob, name }, index) => (
+                            <ImageConvertor
+                                key={crypto.randomUUID()}
+                                image={blob}
+                                fileName={name}
+                                onClickClose={() => {
+                                    setImages(images.filter((image) => image.blob !== blob));
+                                }}
+                            />
+                        ))}
+                    </ul>
+                )}
+            </section>
         </>
     );
 }
