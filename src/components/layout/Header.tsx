8605757a1fbb07dd3f83710a0ffb659e697d9b1c
owen <56942459+cockyb@users.noreply.github.com>
2023-07-06 19:53:27 +0900

    ui: favicon & meta tag

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,11 @@
-import { SlLayers } from 'react-icons/sl';
+import { BiLayerMinus } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
         <header className="px-6 py-4 border-b-gray-200 border-b shadow-sm sticky top-0 bg-white">
             <Link className="flex items-center gap-4" to="/">
-                <SlLayers className="w-6 h-6 hover:text-blue-400 duration-300" />
+                <BiLayerMinus className="w-6 h-6 hover:text-blue-400 duration-300" />
                 <span>
                     Remove <strong className="text-blue-400">Background</strong>
                 </span>
